808d1760581121d4997b3fe142ecc7c5b924b92b
Kelly Doherty <kad2321@yahoo.com>
2024-08-22 17:02:42 -0400

    Setting up some more

--- SW-Dice/src/App.jsx
+++ SW-Dice/src/App.jsx
@@ -1,8 +1,15 @@
 import { useState } from 'react'
+
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
+  return (
+    <>
+      
+    </>
+  )
 }
 
-export default App;
+export default App
